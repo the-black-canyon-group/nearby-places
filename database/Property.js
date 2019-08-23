4cbe3ae093fe6b46cf0af995c3306d9c0f1c1e59
jared <jcraig1730@gmail.com>
2019-08-23 10:38:45 -0700

    refactor database to include zip code and make id a unique value

--- database/Property.js
+++ database/Property.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
+
 const { Schema } = mongoose;
 
-mongoose.connect('mongodb://localhost/fec', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/fec', { useNewUrlParser: true });
 
-PropertySchema = new Schema({
+const PropertySchema = new Schema({
+  id: { type: Number, unique: true },
   location: String,
   price: Number,
   title: String,
   ratings: [Number],
   roomType: String,
-  image: String
+  image: String,
+  zip: Number,
 
-})
+});
 
-module.exports = mongoose.model('Property', PropertySchema)
+module.exports = mongoose.model('Property', PropertySchema);
