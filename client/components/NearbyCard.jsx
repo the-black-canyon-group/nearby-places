1673ac430ac55005a1e8b480144362e831d6ef19
jared <jcraig1730@gmail.com>
2019-08-23 16:24:48 -0700

    setup jest for testing; react barebones for NearbyCard component

---
+++ client/components/NearbyCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NearbyCard extends React.Component {
+  constructor(props) {
+    super(props);
+    thist.state = {};
+  }
+
+}
+
+export default NearbyCard;
---
+++ tests/nearbyCard.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme'
+import NearbyCard from '../client/components/NearbyCard.jsx';
+
+
+test('1 to equal 1', () => {
+  expect(1).toBe(1);
+})
