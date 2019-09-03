81190d5dd5d8134e8c244bb15dfc325cfe6bb551
jared <jcraig1730@gmail.com>
2019-09-03 14:07:16 -0700

    Update component

--- client/index.jsx
+++ client/index.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NearbyService from './components/NearbyService.jsx';
 
-ReactDOM.render(<NearbyService />, document.querySelector('#app'));
+// ReactDOM.render(<NearbyService />, document.querySelector('#app'));
 window.NearbyService = NearbyService;
